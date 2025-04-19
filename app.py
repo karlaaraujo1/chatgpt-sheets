1bd389a6b790d3627ec4d2abc7042de94e1bfa92
karlaaraujo1 <166871945+karlaaraujo1@users.noreply.github.com>
2025-04-18 21:47:31 -0400

    Update app.py

--- app.py
+++ app.py
@@ -70,4 +70,5 @@ def serve_openapi_spec():
     return send_from_directory('.', 'openapi.yaml', mimetype='application/yaml')
 
 if __name__ == '__main__':
-    app.run(port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
